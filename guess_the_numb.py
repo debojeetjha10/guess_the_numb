e81fa90198638dec92880748c58d3e163125fa34
debojeetjha10 <58810504+debojeetjha10@users.noreply.github.com>
2020-03-26 20:13:16 +0530

    Add files via upload

---
+++ guess_the_numb.py
@@ -0,0 +1,43 @@
+#number guessing game
+import random
+
+def getchar():
+   #Returns a single character from standard input
+   import tty, termios, sys
+   fd = sys.stdin.fileno()
+   old_settings = termios.tcgetattr(fd)
+   try:
+      tty.setraw(sys.stdin.fileno())
+      ch = sys.stdin.read(1)
+   finally:
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+   return ch
+guess_number = 0
+print("This is a number guessing game. \nYou will first choose a range of number. \nThen we will generate a secreat number.\nYou have to guess it in minimum number of tries.")
+print("If you enter your guess we will inform you either the number is smaller than the secreat number or bigger.")
+print("RULE : give all input as integer" )
+while getchar() and not guess_number:
+	print("Choose Your Range")
+#print("Your number will be in between :")
+	print("Enter the starting number: ")
+	start = int(input())
+	print("Enter the ending number: ")
+	end = int(input())
+	#guess_number = 0
+	ans = random.randint(start,end)
+	if ans == ValueError:
+		print("Your range is wrong")
+	else:
+		while True:
+			print("Enter your guess")
+			user_input = int(input())
+			guess_number+=1
+			if user_input == ans:
+				print("Congratulations!! You have made it. \n This is the secreat number")
+				break
+			elif user_input > ans:
+				print("Try something small")
+			elif user_input < ans:
+				print("Try something big")
+			else:
+				print("Please follow the rules!!!")
